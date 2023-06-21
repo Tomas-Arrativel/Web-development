6f9a07063ff0d742736cf75716a6af3639fd655e
Tomás Arrativel <arrativeltomas@gmail.com>
2023-06-21 09:43:58 -0300

    first routes

--- backend/3.1 Express/my-express-server/server.js
+++ backend/3.1 Express/my-express-server/server.js
@@ -6,6 +6,16 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
 
+app.get('/contact', (req, res) => {
+  res.send('<p>Contact me at: arrativeltomas@gmail.com</p>');
+});
+
+app.get('/about', (req, res) => {
+  res.send(
+    '<h1>I am Tomás arrativel</h1> <p>I am a Fullstack web developer</p>',
+  );
+});
+
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
