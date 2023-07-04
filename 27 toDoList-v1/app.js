637f8aa1a6ae14cebb622033b4b31fb5635df6eb
Tomás Arrativel <arrativeltomas@gmail.com>
2023-07-04 10:05:59 -0300

    using ejs for the first time

--- 27 toDoList-v1/app.js
+++ 27 toDoList-v1/app.js
@@ -5,11 +5,30 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
+  const days = [
+    'Domingo',
+    'Lunes',
+    'Martes',
+    'Miercoles',
+    'Jueves',
+    'Viernes',
+    'Sabado',
+  ];
   const today = new Date();
-  if (today.getDay === 6 || today.getDay === 0)
-    res.send('Yayy its the weekend!');
-  else res.sendFile(__dirname + '/index.html');
+  const curDay = today.getDay();
+
+  var day = ``;
+
+  if (today.getDay === 6 || today.getDay === 0) {
+    day = `weekend: ${days[curDay]}`;
+  } else {
+    day = `weekday: ${days[curDay]}`;
+  }
+
+  res.render('list', { kindOfDay: day });
 });
 
 app.listen(3000, () => {
