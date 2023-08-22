3189667fb7db0e5156c38f3202e9f7b6e146f1e1
Tomás Arrativel <arrativeltomas@gmail.com>
2023-08-22 10:21:03 -0300

    writing  and reading with node

--- DataBases/fruitsProject/app.js
+++ DataBases/fruitsProject/app.js
@@ -1,17 +1,57 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-
-const url = 'mongodb://localhost:27017';
-
-const dbName = 'fruitsDB';
-
-const client = new MongoClient(url);
-
-client.connect(function (err) {
-  assert.equal(null, err);
-  console.log('Connected successfully to server');
-
-  const db = client.db(dbName);
-
-  client.close();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// Replace the placeholder with your Atlas connection string
+const uri = 'mongodb://localhost:27017';
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+async function run() {
+  try {
+    const database = client.db('fruitsDB');
+    const collection = database.collection('fruits');
+
+    // Insert many files
+    /*  await collection.insertMany([
+      {
+        name: 'Apple',
+        score: 8,
+        review: 'Great fruit',
+      },
+      {
+        name: 'Orange',
+        score: 5,
+        review: 'Kinda sour',
+      },
+      {
+        name: 'Banana',
+        score: 10,
+        review: 'The best fruit ever',
+      },
+    ]);
+
+    console.log(`documents were inserted.`);*/
+
+    // Find Files
+    const findResult = await collection.find();
+    for await (const fruit of findResult) {
+      console.log(fruit);
+    }
+
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!',
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+
+    await client.close();
+  }
+}
+run().catch(console.dir);
